85a3a001aa3e56a3ce0e93bed2201791bf8760f4
袁佳俊 <2752137957@qq.com>
2024-08-19 11:46:54 +0000

    上传代码
    Signed-off-by: 袁佳俊 <2752137957@qq.com>

---
+++ 实验一/pages/index/index.js
@@ -0,0 +1,70 @@
+Page({
+
+
+  data: {
+    src:'/images/微信图片_20240819150735.jpg',
+    name:'Hello World'
+  },
+  getMyInfo : function(e){
+    let info = e.detail.userInfo;
+    this.setData({
+      src: info.avatarUrl,
+      name: info.nickName
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
